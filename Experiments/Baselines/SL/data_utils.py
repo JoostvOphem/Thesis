4006b507f1f2d4141dfb6caacf194da5a887d689
JoostvOphem <joostvanophem@MacBookPro.home>
2025-06-10 13:49:22 +0200

    updated the data utils get dataset functions to get them from the correct model

--- Experiments/Baselines/SL/data_utils.py
+++ Experiments/Baselines/SL/data_utils.py
@@ -4,52 +4,24 @@ import numpy as np
 def load_path_in_tensor(path):
     return torch.tensor(np.array(torch.load(path)))
 
-def get_dataset(dataset_name):
-    if dataset_name == "Ghostbusters_all":
-        train_data = load_path_in_tensor("Datasets/Ghostbusters_standardized_embedded/ghostbusters_ALL_train.jsonl")
-        train_labels = load_path_in_tensor("Datasets/Ghostbusters_standardized_embedded/ghostbusters_ALL_train_labels.pt")
+def get_dataset(dataset_name, roberta_used):
 
-        val_data = load_path_in_tensor("Datasets/Ghostbusters_standardized_embedded/ghostbusters_ALL_val.jsonl")
-        val_labels = load_path_in_tensor("Datasets/Ghostbusters_standardized_embedded/ghostbusters_ALL_val_labels.pt")
-
-        test_data = load_path_in_tensor("Datasets/Ghostbusters_standardized_embedded/ghostbusters_ALL_test.jsonl")
-        test_labels = load_path_in_tensor("Datasets/Ghostbusters_standardized_embedded/ghostbusters_ALL_test_labels.pt")
-    elif dataset_name == "gpt_writing":
-        train_data = load_path_in_tensor("Datasets/Ghostbusters_standardized_embedded/gpt_writing_train.jsonl")
-        train_labels = load_path_in_tensor("Datasets/Ghostbusters_standardized_embedded/gpt_writing_train_labels.pt")
-
-        val_data = load_path_in_tensor("Datasets/Ghostbusters_standardized_embedded/gpt_writing_val.jsonl")
-        val_labels = load_path_in_tensor("Datasets/Ghostbusters_standardized_embedded/gpt_writing_val_labels.pt")
-
-        test_data = load_path_in_tensor("Datasets/Ghostbusters_standardized_embedded/gpt_writing_test.jsonl")
-        test_labels = load_path_in_tensor("Datasets/Ghostbusters_standardized_embedded/gpt_writing_test_labels.pt")
-    elif dataset_name == "monolingual_davinci":
-        train_data = load_path_in_tensor("Datasets/SemEval_standardized_embedded/monolingual/monolingual_davinci_train.jsonl")
-        train_labels = load_path_in_tensor("Datasets/SemEval_standardized_embedded/monolingual/monolingual_davinci_train_labels.pt")
-
-        val_data = load_path_in_tensor("Datasets/SemEval_standardized_embedded/monolingual/monolingual_davinci_val.jsonl")
-        val_labels = load_path_in_tensor("Datasets/SemEval_standardized_embedded/monolingual/monolingual_davinci_val_labels.pt")
-
-        test_data = load_path_in_tensor("Datasets/SemEval_standardized_embedded/monolingual/monolingual_davinci_test.jsonl")
-        test_labels = load_path_in_tensor("Datasets/SemEval_standardized_embedded/monolingual/monolingual_davinci_test_labels.pt")
-    elif dataset_name == "SemEval_complete":
-        train_data = load_path_in_tensor("Datasets/SemEval_standardized_embedded/monolingual/monolingual_complete_train.jsonl")
-        train_labels = load_path_in_tensor("Datasets/SemEval_standardized_embedded/monolingual/monolingual_complete_train_labels.pt")
-
-        val_data = load_path_in_tensor("Datasets/SemEval_standardized_embedded/monolingual/monolingual_complete_val.jsonl")
-        val_labels = load_path_in_tensor("Datasets/SemEval_standardized_embedded/monolingual/monolingual_complete_val_labels.pt")
-
-        test_data = load_path_in_tensor("Datasets/SemEval_standardized_embedded/monolingual/monolingual_complete_test.jsonl")
-        test_labels = load_path_in_tensor("Datasets/SemEval_standardized_embedded/monolingual/monolingual_complete_test_labels.pt")
-    elif dataset_name == "GPT2":
-        train_data = load_path_in_tensor("Datasets/GPT2_standardized_embedded/gpt2_train.jsonl")
-        train_labels = load_path_in_tensor("Datasets/GPT2_standardized_embedded/gpt2_train_labels.pt")
-
-        val_data = load_path_in_tensor("Datasets/GPT2_standardized_embedded/gpt2_val.jsonl")
-        val_labels = load_path_in_tensor("Datasets/GPT2_standardized_embedded/gpt2_val_labels.pt")
-
-        test_data = load_path_in_tensor("Datasets/GPT2_standardized_embedded/gpt2_test.jsonl")
-        test_labels = load_path_in_tensor("Datasets/GPT2_standardized_embedded/gpt2_test_labels.pt")
+    if roberta_used.startswith("monolingual"):
+        start_of_path = f"Datasets/SemEval_standardized_embedded/monolingual/roberta_{roberta_used}"
+    elif roberta_used.startswith("multilingual"):
+        start_of_path = f"Datasets/SemEval_standardized_embedded/multilingual/roberta_{roberta_used}"
+    elif roberta_used.startswith("GPT2"):
+        start_of_path = f"Datasets/GPT2_standardized_embedded/roberta_{roberta_used}"
     else:
-        raise ValueError(f"Unknown dataset: {dataset_name}")
+        start_of_path = f"Datasets/Ghostbusters_standardized_embedded/roberta_{roberta_used}"
+
+    train_data = load_path_in_tensor(f"{start_of_path}/{dataset_name}_train.jsonl")
+    train_labels = load_path_in_tensor(f"{start_of_path}/{dataset_name}_train_labels.pt")
+
+    val_data = load_path_in_tensor(f"{start_of_path}/{dataset_name}_val.jsonl")
+    val_labels = load_path_in_tensor(f"{start_of_path}/{dataset_name}_val_labels.pt")
+
+    test_data = load_path_in_tensor(f"{start_of_path}/{dataset_name}_test.jsonl")
+    test_labels = load_path_in_tensor(f"{start_of_path}/{dataset_name}_test_labels.pt")
+
     return train_data, train_labels, val_data, val_labels, test_data, test_labels
